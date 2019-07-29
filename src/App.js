6a48bf7f1cdf6e7ce6924a39ca7508018a064806
Loic Ndjoyi <lndjoyi@geniuserp.com>
2019-07-29 08:39:48 -0400

    Added bootstrap 4 and updated the schema for test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GuestForm from './GuestForm';
+import React from "react";
+import GuestForm from "./GuestForm";
+import logo from "./logo_inverse.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-light bg-dark">
+          <a className="navbar-brand" href="https://www.geniuserp.com/">
+            <img
+              src={logo}
+              className="d-inline-block align-top"
+              height="52"
+              alt="logo"
+            />
+          </a>
+        </nav>
       </header>
-      <GuestForm />
+      <div className="d-flex flex-column">
+        <h1 className="text-center">Genius Web Data Manager</h1>
+        <div className="card w-75 mx-auto">
+          <div className="card-body">
+            <GuestForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
